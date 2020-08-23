e4680494219e59d7b6902e7205b99efaa27450d7
abner <abner.abbey@hotmail.com>
2020-08-23 11:54:59 -0500

    added color to nav bars

---
+++ TwitterApp/Extensions/+UIColor.swift
@@ -0,0 +1,15 @@
+//
+//  +UIColor.swift
+//  TwitterApp
+//
+//  Created by Abner Castro on 23/08/20.
+//  Copyright © 2020 Abner Castro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static let redMain = UIColor(red: 0.94, green: 0.20, blue: 0.20, alpha: 1.00)
+}
